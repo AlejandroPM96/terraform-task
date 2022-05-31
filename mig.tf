4400ca79a0a5502f625ec6c95b25a95ebe3b7447
PenaPC <luis_alejandro241096@hotmail.com>
2022-05-31 10:47:48 -0500

    adding tf files

---
+++ mig.tf
@@ -0,0 +1,77 @@
+provider "google" {
+  project = var.project
+  credentials = file(var.credentials_file)
+}
+
+provider "google-beta" {
+  project = var.project
+  credentials = file(var.credentials_file)
+}
+
+module "mig1_template" {
+  source     = "terraform-google-modules/vm/google//modules/instance_template"
+  version    = "6.2.0"
+  network    = google_compute_network.default.self_link
+  subnetwork = google_compute_subnetwork.group1.self_link
+  service_account = {
+    email  = ""
+    scopes = ["cloud-platform"]
+  }
+  name_prefix          = "${var.network_prefix}-group1"
+  startup_script       = file(var.script_path)
+  source_image_family  = "ubuntu-1804-lts"
+  source_image_project = "ubuntu-os-cloud"
+  tags = [
+    "${var.network_prefix}-group1",
+    module.cloud-nat-group1.router_name
+  ]
+}
+
+module "mig1" {
+  source            = "terraform-google-modules/vm/google//modules/mig"
+  version           = "6.2.0"
+  instance_template = module.mig1_template.self_link
+  region            = var.group1_region
+  hostname          = "${var.network_prefix}-group1"
+  target_size       = var.target_size
+  named_ports = [{
+    name = "http",
+    port = 80
+  }]
+  network    = google_compute_network.default.self_link
+  subnetwork = google_compute_subnetwork.group1.self_link
+}
+
+module "mig2_template" {
+  source     = "terraform-google-modules/vm/google//modules/instance_template"
+  version    = "6.2.0"
+  network    = google_compute_network.default.self_link
+  subnetwork = google_compute_subnetwork.group2.self_link
+  service_account = {
+    email  = ""
+    scopes = ["cloud-platform"]
+  }
+  name_prefix    = "${var.network_prefix}-group2"
+  startup_script = file(var.script_path)
+  source_image_family  = "ubuntu-1804-lts"
+  source_image_project = "ubuntu-os-cloud"
+  tags = [
+    "${var.network_prefix}-group2",
+    module.cloud-nat-group2.router_name
+  ]
+}
+
+module "mig2" {
+  source            = "terraform-google-modules/vm/google//modules/mig"
+  version           = "6.2.0"
+  instance_template = module.mig2_template.self_link
+  region            = var.group2_region
+  hostname          = "${var.network_prefix}-group2"
+  target_size       = var.target_size
+  named_ports = [{
+    name = "http",
+    port = 80
+  }]
+  network    = google_compute_network.default.self_link
+  subnetwork = google_compute_subnetwork.group2.self_link
+}
---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "project" {
+  type = string
+  default = "terraform-351519"
+}
+
+variable "credentials_file" { 
+  type = string
+  default = "../terraform-351519-db745207ceec.json"
+}
+
+variable "target_size" {
+  type    = number
+  default = 2
+}
+
+variable "group1_region" {
+  type    = string
+  default = "us-central1"
+}
+
+variable "group2_region" {
+  type    = string
+  default = "us-east1"
+}
+
+variable "network_prefix" {
+  type    = string
+  default = "multi-mig-lb-http"
+}
+
+variable "script_path" {
+  type        = string
+  default = "./startup.sh"
+}
